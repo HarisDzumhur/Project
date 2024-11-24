8ab6072baa6de4cbe58208851d1b228ae2f66f91
HarisDzumhur <harisdzumhur4@gmail.com>
2024-11-24 12:40:21 +0100

    init

---
+++ FootballLeague/FootballLeague/Service.Interfaces/INavigationService.cs
@@ -0,0 +1,16 @@
+﻿using FootballLeague.Viewmodels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FootballLeague.Service.Interfaces
+{
+    public interface INavigationService
+    {
+        ViewModelBase CurrentView { get; }
+        void NavigateTo<T>() where T : ViewModelBase;
+        void NavigateTo<T>(object parameters) where T : ViewModelBase;
+    }
+}
---
+++ FootballLeague/FootballLeague/Viewmodels/ViewModelBase.cs
@@ -0,0 +1,21 @@
+﻿using FootballLeague.Core;
+using FootballLeague.Service.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FootballLeague.Viewmodels
+{
+    public abstract class ViewModelBase : ObservableObject
+    {
+        private INavigationService _navigationService;
+
+        public INavigationService NavigationService
+        {
+            get { return _navigationService; }
+            set { _navigationService = value; OnPropertyChanged(); }
+        }
+    }
+}
